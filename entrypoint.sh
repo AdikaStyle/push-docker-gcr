f85db317be7d82a2a2fe084e3fc0cfd8fd98f0fd
Domrev Igor <igor.domrev@tg-17.com>
2020-08-27 16:51:20 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ echo $IMAGE_NAME
 echo banana
 echo $INPUT_GCR_PROJECT_ID
 echo duck
-echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
+echo https://$INPUT_GCR_HOST/
+echo -n $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
 docker build -t $IMAGE_NAME .
 docker images
 docker push $IMAGE_NAME

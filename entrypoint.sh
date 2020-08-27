3f996e43d066c9dc543c6a0539e8b5bad45a331c
Domrev Igor <igor.domrev@tg-17.com>
2020-08-27 17:35:32 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
 IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
+LATEST_IMAGE="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT_ID/$INPUT_IMAGE_NAME:latest"
+
 echo -n $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -t $LATEST_IMAGE .
 docker push $IMAGE_NAME
+docker push $LATEST_IMAGE

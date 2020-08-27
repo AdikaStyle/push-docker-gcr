3ba039153056c82dc1586361edcb2cc9a34b8221
Domrev Igor <igor.domrev@tg-17.com>
2020-08-27 15:27:34 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TA
 echo $IMAGE_NAME
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
 docker build -t $IMAGE_NAME .
+docker images
 docker push $IMAGE_NAME

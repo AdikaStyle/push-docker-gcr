d830a1813d68c43394c9c5b08628f1aa62f7ba5b
matang <matang28@gmail.com>
2021-03-11 14:54:20 +0200

    Reverting back to old version

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,5 @@ IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE
 
 echo -n $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
 
-ls -la
-cd $INPUT_WORKING_DIR
-
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
